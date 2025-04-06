2e2a7df4638747f06a0fd84f53364391836a75cb
Areesha Faisal <91604677+psdcoderr@users.noreply.github.com>
2025-04-06 15:37:43 +0500

    Delete Downloads/Devops-Assignment-1-Areesha-Baji directory

--- Downloads/Devops-Assignment-1-Areesha-Baji/frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders frontend heading', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/react frontend/i);
-  expect(linkElement).toBeInTheDocument();
-});
